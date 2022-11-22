5f42d276da7bb187163eb500cb1ea3cab9634519
mohamedElsayedIbrahim <mohamdeesayed@outlook.com>
2022-11-22 15:15:18 +0200

    fixing

---
+++ database/migrations/2022_11_22_091121_create_sliders_table.php
@@ -0,0 +1,35 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateSlidersTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('sliders', function (Blueprint $table) {
+            $table->id();
+            $table->string('image',200);
+            $table->string('content',200);
+            $table->string('btn_value',50);
+            $table->string('link',250)->default('#');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('sliders');
+    }
+}
